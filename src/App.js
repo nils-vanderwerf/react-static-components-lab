77d7c690fce54bcf7e2f796de60fd3aa94139a3d
Nils van der Werf <n.vanderw.92@gmail.com>
2022-02-11 15:51:12 +1100

    Imported components correctly

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import CatComponent from './CatComponent'
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent'
 
 class App extends Component {
   render() {
